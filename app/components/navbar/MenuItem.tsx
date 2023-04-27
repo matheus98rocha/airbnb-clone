8b2976f533578cee7fdcb6634b8394ae40ff74d6
matheus98rocha <matheus98rocha@gmail.com>
2023-04-26 23:00:18 -0300

    feature - menu ui

---
+++ app/components/navbar/MenuItem.tsx
@@ -0,0 +1,19 @@
+'use client'
+
+interface MenuItemProps {
+  onClick: () => void
+  label: string
+}
+
+function MenuItem({ onClick, label }: MenuItemProps) {
+  return (
+    <div
+      className="px-4 py-3 hover:bg-neutral-100 transition font-semibold"
+      onClick={onClick}
+    >
+      {label}
+    </div>
+  )
+}
+
+export default MenuItem
