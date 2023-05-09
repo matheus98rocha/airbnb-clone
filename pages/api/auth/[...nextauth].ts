02f2f972a2b37c3acfe19a24da03cd2bcbb853ef
matheus98rocha <matheus98rocha@gmail.com>
2023-05-08 23:52:26 -0300

    fix - next adapter correction

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,10 +1,11 @@
-import { PrismaAdapter } from '@next-auth/prisma-adapter';
-import NextAuth, { AuthOptions } from 'next-auth'
-import CredentialsProvider from 'next-auth/providers/credentials'
-import GithubProvider from 'next-auth/providers/github'
-import GoogleProvider from 'next-auth/providers/google'
+import bcrypt from "bcrypt"
+import NextAuth, { AuthOptions } from "next-auth"
+import CredentialsProvider from "next-auth/providers/credentials"
+import GithubProvider from "next-auth/providers/github"
+import GoogleProvider from "next-auth/providers/google"
+import { PrismaAdapter } from "@next-auth/prisma-adapter"
 
-import bcrypt from "bcrypt";
+import prisma from "../../../app/libs/prsimadb"
 
 export const authOptions: AuthOptions = {
   adapter: PrismaAdapter(prisma),
